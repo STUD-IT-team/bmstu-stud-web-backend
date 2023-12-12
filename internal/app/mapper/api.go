0c576df08e687531cb7b9e410eb70a399ed498c5
thirteenths <vera.korotkaya@yandex.ru>
2023-12-12 23:13:26 +0300

    create feedHandler

--- internal/app/mapper/api.go
+++ internal/app/mapper/api.go
@@ -1,28 +1,11 @@
 package mapper
 
 import (
-	"github.com/STUD-IT-team/bmstu-stud-web-backend/internal/domain"
 	"github.com/STUD-IT-team/bmstu-stud-web-backend/internal/domain/responses"
 )
 
 func CreateEcho() *responses.GetEcho {
 	return &responses.GetEcho{
 		Message: "echo",
-	}
-}
-
-func MakeResponseAllFeed(dom []domain.Feed) *[]responses.GetAllFeed {
-	res := []responses.GetAllFeed{}
-	for _, i := range dom {
-		res = append(res, responses.GetAllFeed{
-			Id: i.Id, Title: i.Title, Description: i.Description,
-		})
-	}
-	return &res
-}
-
-func MakeResponseFeed(dom domain.Feed) *responses.GetFeed {
-	return &responses.GetFeed{
-		Id: dom.Id, Title: dom.Title, Description: dom.Description, RegistationURL: dom.RegistationURL,
 	}
 }
