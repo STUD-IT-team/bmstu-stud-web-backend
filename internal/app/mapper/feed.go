cf4cd1d8a82371a96fb7a984e459c88770d2cb5f
Antony Pavlenko <antony.pavlenko@anec.app>
2024-02-11 19:42:02 +0300

    added domain structures changed feed

--- internal/app/mapper/feed.go
+++ internal/app/mapper/feed.go
@@ -1,6 +1,7 @@
 package mapper
 
 import (
+	"encoding/base64"
 	"github.com/STUD-IT-team/bmstu-stud-web-backend/internal/domain"
 	"github.com/STUD-IT-team/bmstu-stud-web-backend/internal/domain/responses"
 )
@@ -10,12 +11,15 @@ func MakeResponseAllFeed(f []domain.Feed) *responses.GetAllFeed {
 	for _, i := range f {
 		feed = append(feed,
 			responses.Feed{
-				ID:          i.ID,
-				Title:       i.Title,
-				Description: i.Description,
+				ID:              i.ID,
+				Title:           i.Title,
+				Description:     i.Description,
+				RegistrationURL: i.RegistrationURL,
+				Media:           base64.Encoding{},
+				CreatedBy:       i.CreatedBy,
+				UpdatedAt:       i.UpdatedAt,
 			})
 	}
-
 	return &responses.GetAllFeed{Feed: feed}
 }
 
@@ -25,5 +29,8 @@ func MakeResponseFeed(f domain.Feed) *responses.GetFeed {
 		Title:           f.Title,
 		Description:     f.Description,
 		RegistrationURL: f.RegistrationURL,
+		Media:           base64.Encoding{},
+		CreatedBy:       f.CreatedBy,
+		UpdatedAt:       f.UpdatedAt,
 	}
 }
--- internal/domain/feed.go
+++ internal/domain/feed.go
@@ -1,8 +1,16 @@
 package domain
 
+import "time"
+
 type Feed struct {
-	ID              int    `json:"id"`
-	Title           string `json:"title"`
-	Description     string `json:"description"`
-	RegistrationURL string `json:"registration_url"`
+	ID              int       `db:"id"`
+	Title           string    `db:"title"`
+	Description     string    `db:"description"`
+	RegistrationURL string    `db:"registration_url"`
+	Approved        bool      `db:"approved"`
+	MediaUrl        string    `db:"media_url"`
+	UpdatedAt       time.Time `db:"updated_at"`
+	CreatedAt       time.Time `db:"created_at"`
+	CreatedBy       int       `db:"created_by"`
+	Views           int       `db:"views"`
 }
