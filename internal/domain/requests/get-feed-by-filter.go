535a7c3ef6f8154b421ffa28b9d29e742b8fca15
thirteenths <vera.korotkaya@yandex.ru>
2024-04-13 20:56:01 +0300

    SWP-ca5-feed: used mo

--- internal/domain/requests/get-feed-by-filter.go
+++ internal/domain/requests/get-feed-by-filter.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+
+	"github.com/samber/mo"
 )
 
 type GetFeedByFilter struct {
@@ -17,40 +19,34 @@ func (f *GetFeedByFilter) Bind(req *http.Request) error {
 	var limit, offset int
 	var err error
 
-	if query.Has("offset") {
-		offset, err = strconv.Atoi(query.Get("offset"))
+	stringOffset := query.Get("offset")
+	if optionOffset := mo.TupleToOption(stringOffset, stringOffset != ""); optionOffset.IsPresent() {
+		offset, err = strconv.Atoi(optionOffset.MustGet())
 		if err != nil {
 			return fmt.Errorf("can't Atoi offset on GetAllFeed.Bind: %w", err)
 		}
-
-		if offset < 0 {
-			return fmt.Errorf("require: offset < 0")
-		}
 	}
 
-	if query.Has("limit") {
-		limit, err = strconv.Atoi(query.Get("limit"))
+	stringLimit := query.Get("limit")
+	if optionLimit := mo.TupleToOption(stringLimit, stringLimit != ""); optionLimit.IsPresent() {
+		limit, err = strconv.Atoi(optionLimit.MustGet())
 		if err != nil {
 			return fmt.Errorf("can't Atoi limit on GetAllFeed.Bind: %w", err)
-		}
-
-		if limit < 0 {
-			return fmt.Errorf("require: limit < 0")
 		}
 	}
 
 	f.Offset = offset
 	f.Limit = limit
 
-	return nil
+	return f.GetLimitOffset()
 }
 
 func (f *GetFeedByFilter) GetLimitOffset() error {
-	if f.Offset <= 0 {
+	if f.Offset < 0 {
 		return fmt.Errorf("require: offset <= 0")
 	}
 
-	if f.Limit <= 0 {
+	if f.Limit < 0 {
 		return fmt.Errorf("require: limit <= 0")
 	}
 
