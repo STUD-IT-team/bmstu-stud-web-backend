dc4698edeb8dd4abe8ea5ae99a6750db9c84382c
pai0id <andrey.polaykov05@gmail.com>
2024-07-05 10:38:34 +0300

    image to image_url rename

--- migrations/008_alter_media_tbl.sql
+++ migrations/008_alter_media_tbl.sql
@@ -1,8 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
+ALTER table mediafile rename column image to image_url;
 
-ALTER table mediafile ALTER column image type TEXT;
-update mediafile set image = 'localhost:5000/about/arch.png';
+ALTER table mediafile ALTER column image_url type TEXT;
+update mediafile set image_url = 'localhost:5000/about/arch.png';
 
 -- +goose StatementEnd
 
