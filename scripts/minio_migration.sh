20039913a585cae2ac89d5fea79a89e2f888fb80
pai0id <andrey.polaykov05@gmail.com>
2024-07-09 15:36:15 +0300

    minio fix

--- scripts/minio_migration.sh
+++ scripts/minio_migration.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-mc config host add minio http://172.18.0.2:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
+mc config host add minio http://minio:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 mc mb --ignore-existing minio/videos
 mc anonymous set public minio/videos
+mc mb --ignore-existing minio/images
+mc anonymous set public minio/images
 mc put data/main_vid.mp4 minio/videos
-mc put data/arch.png minio/videos
+mc put data/arch.png minio/images
