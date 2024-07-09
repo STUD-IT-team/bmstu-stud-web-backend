82cecf5915eddcbd14eabda5235603f744afaa1d
pai0id <andrey.polaykov05@gmail.com>
2024-07-09 13:31:00 +0300

    pricrutil minio

---
+++ scripts/minio_migration.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mc config host add minio http://172.18.0.2:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
+mc mb --ignore-existing minio/videos
+mc anonymous set public minio/videos
+mc put data/main_vid.mp4 minio/videos
