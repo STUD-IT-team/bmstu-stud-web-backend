cab4df30f33021eafa709fdf48e1b98de90223cd
pai0id <andrey.polaykov05@gmail.com>
2024-07-09 13:57:40 +0300

    add png

--- scripts/minio_migration.sh
+++ scripts/minio_migration.sh
@@ -4,3 +4,4 @@ mc config host add minio http://172.18.0.2:9000 $MINIO_ROOT_USER $MINIO_ROOT_PAS
 mc mb --ignore-existing minio/videos
 mc anonymous set public minio/videos
 mc put data/main_vid.mp4 minio/videos
+mc put data/arch.png minio/videos
