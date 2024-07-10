b952ea071f7d9d85b5a8f2b54e4dbce5aed51725
Impervguin <leon53000@yandex.ru>
2024-07-10 11:31:10 +0300

    Moved bucker to env file

--- scripts/minio_migration.sh
+++ scripts/minio_migration.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 mc config host add minio http://minio:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
-mc mb --ignore-existing minio/videos
-mc anonymous set public minio/videos
-mc mb --ignore-existing minio/images
-mc anonymous set public minio/images
-mc put data/main_vid.mp4 minio/videos
-mc put data/arch.png minio/images
+mc mb --ignore-existing minio/"$VIDEO_BUCKET"
+mc anonymous set public minio/"$VIDEO_BUCKET"
+mc mb --ignore-existing minio/"$IMAGE_BUCKET"
+mc anonymous set public minio/"$IMAGE_BUCKET"
+mc mb --ignore-existing minio/"$DOCUMENT_BUCKET"
+mc anonymous set public minio/"$DOCUMENT_BUCKET"
+mc put data/main_vid.mp4 minio/"$VIDEO_BUCKET"
+mc put data/arch.png minio/"$IMAGE_BUCKET"
