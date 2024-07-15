10dbf26ac37f886ffda2d2743d391adfe91ef5e2
pai0id <andrey.polaykov05@gmail.com>
2024-07-15 13:07:42 +0300

    new err wrap

--- internal/infrastructure/postgres/errors.go
+++ internal/infrastructure/postgres/errors.go
@@ -3,14 +3,20 @@ package postgres
 import (
 	"errors"
 	"fmt"
+
+	"github.com/jackc/pgx"
 )
 
-const uniqueConstraintViolation string = "23505"
-const foreignKeyViolation string = "23503"
+const (
+	uniqueConstraintViolation string = "23505"
+	foreignKeyViolation       string = "23503"
+	notFoundError             string = "20000"
+)
 
 var (
 	TextPostgresUniqueConstraintViolation = "postgres unique constraint violation"
 	TextPostgresForeignKeyViolation       = "postgres foreign key violation"
+	TextPostgresNotFoundError             = "postgres not found error"
 	TextPostgresUnknownError              = "postgres unknown error"
 )
 
@@ -18,6 +24,7 @@ var (
 	ErrPostgresUniqueConstraintViolation = errors.New(TextPostgresUniqueConstraintViolation)
 	ErrPostgresForeignKeyViolation       = errors.New(TextPostgresForeignKeyViolation)
 	ErrPostgresUnknownError              = errors.New(TextPostgresUnknownError)
+	ErrPostgresNotFoundError             = errors.New(TextPostgresNotFoundError)
 )
 
 func mapPostgresError(code string) error {
@@ -27,12 +34,19 @@ func mapPostgresError(code string) error {
 	if code == foreignKeyViolation {
 		return ErrPostgresForeignKeyViolation
 	}
+	if code == notFoundError {
+		return ErrPostgresNotFoundError
+	}
 	return ErrPostgresUnknownError
 }
 
-func wrapPostgresError(code string, err error) error {
+func wrapPostgresError(err error) error {
 	if err == nil {
 		return nil
 	}
-	return fmt.Errorf("%s %w: %w", code, mapPostgresError(code), err)
+	perr, ok := err.(pgx.PgError)
+	if !ok {
+		return err
+	}
+	return fmt.Errorf("%s %w: %w", perr.Code, mapPostgresError(perr.Code), err)
 }
