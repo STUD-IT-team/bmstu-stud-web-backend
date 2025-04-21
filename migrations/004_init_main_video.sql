38929077487d6e28770f9ace1238f3a23101a5b6
impervguin <leon53000@yandex.ru>
2025-04-22 01:13:25 +0300

    quick migration fix

--- migrations/004_init_main_video.sql
+++ migrations/004_init_main_video.sql
@@ -8,11 +8,11 @@ create table IF NOT EXISTS main_video
     key     text DEFAULT '',
     club_id int  NOT NULL,
     current boolean default NULL,
-    FOREIGN KEY (club_id) REFERENCES club(id),
+    FOREIGN KEY (club_id) REFERENCES club(id)
 );
 
 CREATE UNIQUE INDEX ON main_video (club_id, current)
-WHERE active = TRUE;
+WHERE current = TRUE;
 
 -- +goose StatementEnd
 
